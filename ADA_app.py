4cc23ecbb82be50431905977de288be7bda72351
mirinaim <mim@wm.edu>
2025-09-27 18:41:55 -0400

    added frontend
    added frontend to project

--- ADA_app.py
+++ ADA_app.py
@@ -7,7 +7,6 @@ from shapely.geometry import Point
 
 cvs_loc = "locations.csv"   
 # Add your Mapbox token here (you can get one from https://account.mapbox.com/access-tokens/)
-mapbox_token = "INSERT_TOKEN"
 
 # Initialize the Dash app
 app = Dash(__name__)
@@ -150,4 +149,3 @@ def update_image(clickData):
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8052)
